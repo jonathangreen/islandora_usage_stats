f67b0856288e73f76456a65f5a2fee1258b2b2f9
Mark Jordan <mjordan@sfu.ca>
2018-03-06 01:41:46 +0000

    Address 7.x-ISLANDORA-2170.

--- islandora_usage_stats.api.php
+++ islandora_usage_stats.api.php
@@ -11,14 +11,14 @@
  * attempt to persist the event failed, 
  *
  * @param string $type
- *   One of 'object', 'collection', 'cmodel', 'ds'.
+ *   One of 'object', 'parent_collection', 'cmodel', 'ds'.
  * @param int $access_id
  *   The database ID resulting from persisting the entry to the database.
  *   If the attempt to persist the record failed, this will be NULL.
  * @param int $pid_id
  *   For object access events, the ID of the row recording the event.
  * @param string $pid
- *   For object access events, the objec't PID.
+ *   For object access events, the object's PID.
  */
 function hook_islandora_usage_stats_access_recorded($type, $access_id, $pid_id = NULL, $pid = NULL);
 
